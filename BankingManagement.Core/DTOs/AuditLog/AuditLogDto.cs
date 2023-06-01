f581111c4657a4c27eb3e323a382b28604106c01
Turan Öz <turan.oz@armagroupholding.com>
2023-06-01 16:04:57 +0300

    updated

--- BankingManagement.Core/DTOs/AuditLog/AuditLogDto.cs
+++ BankingManagement.Core/DTOs/AuditLog/AuditLogDto.cs
@@ -4,6 +4,7 @@ public class AuditLogDto
 {
     public Guid AuditLogId { get; set; }
     public string Action { get; set; }
+    public string IPAddress { get; set; }
     public DateTime ActionTime { get; set; }
     public Guid UserId { get; set; }
 }
