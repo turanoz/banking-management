b3a549900d505289d498399718537cf3d0b710ad
Turan Öz <turan.oz@armagroupholding.com>
2023-06-01 16:11:34 +0300

    updated

---
+++ BankingManagement.Repository/Migrations/20230531172455_account-updated.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankingManagement.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class accountupdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AccountType",
+                table: "Accounts",
+                newName: "Type");
+
+            migrationBuilder.RenameColumn(
+                name: "AccountNumber",
+                table: "Accounts",
+                newName: "Number");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Accounts");
+
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Accounts",
+                newName: "AccountType");
+
+            migrationBuilder.RenameColumn(
+                name: "Number",
+                table: "Accounts",
+                newName: "AccountNumber");
+        }
+    }
+}
